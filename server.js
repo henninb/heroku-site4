be09bb08f7c2765aaa153d97d51a9edd88a03424
Brian Henning <brian.henning@humansecurity.com>
2022-09-23 08:19:37 -0500

    wip

--- server.js
+++ server.js
@@ -8,8 +8,6 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-/* PerimeterX Bot Defender Config */
-
 const pxConfig = {
     px_app_id: process.env.PX_APP_ID,
     px_cookie_secret: process.env.PX_COOKIE_SECRET,
@@ -17,14 +15,9 @@ const pxConfig = {
     px_module_mode: "active_blocking",
     px_enforced_routes: ['/protected'],
     px_monitored_routes: ['/monitored'],
-    // px_additional_activity_handler: function(pxCtx, config) {
-    //     console.log(pxCtx);
-    // },
     px_logger_severity: 'debug',
-    // px_bypass_monitor_header: "x-px-block",
-    // px_blocking_score: 0
 };
-/* Initialize Defender */
+
 perimeterx.init(pxConfig);
 
 app.use(express.static('public'));
@@ -32,10 +25,6 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Credentials', 'true')
     next();
 });
-
-// app.get('/', function (req, res) {
-//     res.sendFile(path.join(__dirname, '/index.html'));
-// });
 
 app.get('/protected', perimeterx.middleware, (req, res) => {
     res.sendFile(path.join(__dirname, '/public/protected.html'));
@@ -46,4 +35,4 @@ app.get('/monitored', perimeterx.middleware, (req, res) => {
 });
 
 app.listen(port);
-console.log('Server started at http://localhost:' + port);
+console.log('Server started at port: ' + port);
